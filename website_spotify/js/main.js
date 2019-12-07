8ef75306273c3c2c6b62e6607520c1e50f50bc25
sethbilliau <sethbilliau@gmail.com>
2019-12-07 16:01:59 -0500

    index

--- website_spotify/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#pagepiling').pagepiling({
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage'],
-        menu: '#myMenu',
-        scrollingSpeed: 500,
-    });
-});

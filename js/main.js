38ef066258b2594afe2ae38da5308c1684c4e4b9
sethbilliau <sethbilliau@gmail.com>
2019-12-09 12:38:28 -0500

    index updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage'],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage'],
         menu: '#myMenu',
         scrollingSpeed: 500,
     });
